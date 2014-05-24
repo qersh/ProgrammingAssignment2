df20cbc7ed0c9c5574837ab4a63baeaafd2d136e
Mohammad Al Qersh <malqersh@qersh.com>
2014-05-24 20:39:05 +0300

    Solved the function. and Added comments to explain how the function work

--- cachematrix.R
+++ cachematrix.R
@@ -5,18 +5,27 @@
 ## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
+    # Init z with NULL value
+    z <- NULL
+    
+    # When calling "set" mackCaheMatrix "x" value to be like "y"
+    # and set "z" value to NULL
     set <- function(y) {
         x <<- y
-        m <<- NULL
+        z <<- NULL
     }
+    
+    # return "x" value when called
     get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
+    # set the inverse of the matrix using solve 
+    setInverse <- function(solve) z <<- solve
+    # return the inverse value stored in "z"
+    getInverse <- function() z
+    
+    # List of functions in this function
     list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
-
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
@@ -25,14 +34,21 @@ makeCacheMatrix <- function(x = matrix()) {
 ## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-    m <- x$getmean()
-    if(!is.null(m)) {
+    ## Return a matrix that is the inverse of 'x'
+    # call getInverse Function and assign the value to "z"
+    z <- x$getInverse()
+    # check if the Inverse already set
+    if(!is.null(z)) {
+        # if yes write a message and return "z"
         message("getting cached data")
-        return(m)
+        return(z)
     }
+    # get the value and assign to data
     data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
+    # use solve to Inverse the matrix
+    z <- solve(data, ...)
+    # call the function to set the Inverse value (to use it later)
+    x$setInverse(z)
+    # return the inverse value 
+    z
 }
